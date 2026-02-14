bc388d91eea2dcd3f6a667f72ca70ee97af1eb78
harishankarprajapati <pharishankar1453@gmail.com>
2026-02-14 18:03:49 +0530

    gjkgfhkjlgfh

--- js/settings.js
+++ js/settings.js
@@ -123,4 +123,11 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+// scroolbar
+    window.onscroll = function () {
+      let winScroll = document.documentElement.scrollTop || document.body.scrollTop;
+      let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      let scrolled = (winScroll / height) * 100;
+      document.getElementById("scrollProgress").style.width = scrolled + "%";
+    };
 
